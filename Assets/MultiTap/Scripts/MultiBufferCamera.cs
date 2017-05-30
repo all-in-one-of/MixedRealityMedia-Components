c847d3a99cc80ae08637f99b09b4c7e61c9a7bf1
Mio Bambino <s59330@beuth-hochschule.de>
2017-05-30 20:02:58 +0200

    Included multi tap render textures.

---
+++ Assets/MultiTap/Scripts/MultiBufferCamera.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class MultiBufferCamera : MonoBehaviour {
+
+	public RenderTexture depthTexture;
+	public RenderTexture rgbaTexture;
+
+	void OnEnable () {
+		if(depthTexture == null) {
+			Debug.LogError("No depth texture set");
+			return;
+		}
+		if(rgbaTexture == null) {
+			Debug.LogError("No RGBA texture set");
+			return;
+		}
+
+		var cam = GetComponent<Camera>();
+		if(cam == null) {
+			Debug.LogError("Cannot find Camera on gameObject");
+			return;
+		}
+
+		cam.SetTargetBuffers(rgbaTexture.colorBuffer, depthTexture.depthBuffer);
+		Debug.Log("Finished setting up all buffers.");
+	}
+}
