90f7c421b27059fdbc5259b3332e1e442f46eacd
Martin Michael Zier <martin.michael.zier@artcom.de>
2017-06-16 16:52:57 +0200

    focal length calculation added

--- Assets/01_Stencil Delayed ZSort/Scripts/CameraManager.cs
+++ Assets/01_Stencil Delayed ZSort/Scripts/CameraManager.cs
@@ -4,13 +4,52 @@ using UnityEngine;
 
 public class CameraManager : MonoBehaviour {
 
-	// Use this for initialization
+	public List<Camera> managedCameras;
+	public enum LensType {
+		RectliniarFilm,
+		RectliniarDSLR
+	}
+	public LensType type = LensType.RectliniarFilm;
+	public float focalLength;
+	[HideInInspector, SerializeField]
+	public float FocalLength {
+		get {
+			return focalLength;
+		}
+		set {
+			focalLength = value;
+			RecalculateFOV();
+		}
+	}
+	public Vector2 SensorSize {
+		get {
+			return sensorSize;
+		}
+		set {
+			sensorSize = value;
+			RecalculateFOV();
+		}
+	}
+	[HideInInspector, SerializeField]
+	public Vector2 sensorSize;
+	public float fieldOfView;
+	public Transform rootTransform;
+	public Transform controllerTransform;
+
 	void Start () {
-		
+		RecalculateFOV();
+	}
+
+	void Update() {
+		rootTransform.position = controllerTransform.position;
+		rootTransform.rotation = controllerTransform.rotation;
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		
+	private void RecalculateFOV() {
+		double fovdub = Mathf.Rad2Deg * 2.0 * Mathf.Atan(sensorSize.y  / (2f * focalLength));
+		fieldOfView = (float) fovdub;
+		foreach(Camera c in managedCameras) {
+			c.fieldOfView = fieldOfView;
+		}
 	}
 }
